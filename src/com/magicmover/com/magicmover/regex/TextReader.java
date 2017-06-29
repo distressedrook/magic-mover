c3579f27930e408015f4ab9700af0858c366c789
raghunandan@innoflexion.com <nandan143>
2017-06-29 12:16:52 +0530

    Added text reader

--- src/com/magicmover/com/magicmover/regex/TextReader.java
+++ src/com/magicmover/com/magicmover/regex/TextReader.java
@@ -9,45 +9,27 @@ public class TextReader {
     private static final String FILENAME = "D:\\test\\filename.txt";
 
     public static void main(String[] args) {
-
         BufferedReader br = null;
         FileReader fr = null;
-
         try {
-
             fr = new FileReader(FILENAME);
             br = new BufferedReader(fr);
-
             String sCurrentLine;
-
             br = new BufferedReader(new FileReader(FILENAME));
-
             while ((sCurrentLine = br.readLine()) != null) {
                 System.out.println(sCurrentLine);
             }
-
         } catch (IOException e) {
-
             e.printStackTrace();
-
         } finally {
-
             try {
-
                 if (br != null)
                     br.close();
-
                 if (fr != null)
                     fr.close();
-
             } catch (IOException ex) {
-
                 ex.printStackTrace();
-
             }
-
         }
-
     }
-
 }
