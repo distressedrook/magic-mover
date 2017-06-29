83eeb9105fcf2e08ff2944b6f1fad59ca074ea25
raghunandan@innoflexion.com <nandan143>
2017-06-29 12:11:25 +0530

    Added text reader

---
+++ src/com/magicmover/com/magicmover/regex/TextReader.java
@@ -0,0 +1,53 @@
+package com.magicmover.com.magicmover.regex;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TextReader {
+
+    private static final String FILENAME = "D:\\test\\filename.txt";
+
+    public static void main(String[] args) {
+
+        BufferedReader br = null;
+        FileReader fr = null;
+
+        try {
+
+            fr = new FileReader(FILENAME);
+            br = new BufferedReader(fr);
+
+            String sCurrentLine;
+
+            br = new BufferedReader(new FileReader(FILENAME));
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                System.out.println(sCurrentLine);
+            }
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            try {
+
+                if (br != null)
+                    br.close();
+
+                if (fr != null)
+                    fr.close();
+
+            } catch (IOException ex) {
+
+                ex.printStackTrace();
+
+            }
+
+        }
+
+    }
+
+}
